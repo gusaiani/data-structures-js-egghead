76fcd8b1855e210103f814cdd9e59a2a9ec69c77
gusaiani <gustavo@poe.ma>
2020-03-18 07:59:12 -0300

    Cover binaryTree with tests

--- tests/binaryTree.test.js
+++ tests/binaryTree.test.js
@@ -1,17 +1,40 @@
 const createBinaryTree = require('../binaryTree')
 
 describe('binaryTree', () => {
-  test('should pass sanity check', () => {
-    const tree = createBinaryTree('a')
-    const b = tree.root.addLeft('b')
-    const c = tree.root.addRight('c')
-    const d = b.addLeft('d')
-    const e = b.addRight('e')
-    const f = c.addLeft('f')
-    const g = c.addRight('g')
-    const h = d.addLeft('h')
-    const i = d.addRight('i')
+  /*
+   * a
+   *   b
+   *     d
+   *       h
+   *       i
+   *     e
+   *   c
+   *     f
+   *     g
+  */
+  const tree = createBinaryTree('a')
+  const b = tree.root.addLeft('b')
+  const c = tree.root.addRight('c')
+  const d = b.addLeft('d')
+  b.addRight('e')
+  c.addLeft('f')
+  c.addRight('g')
+  d.addLeft('h')
+  d.addRight('i')
 
-    console.log(tree.print());
+  test('should show h first if traversed in order', () => {
+    expect(tree.print().split(' => ')[0]).toBe('h')
+  })
+
+  test('should show a first if traversed pre order', () => {
+    expect(tree.print('PRE_ORDER').split(' => ')[0]).toBe('a')
+  })
+
+  test('should show a first if traversed post order', () => {
+    const treePrintArray = tree.print('POST_ORDER').split(' => ')
+
+    expect(treePrintArray[0]).toBe('h')
+    expect(treePrintArray[1]).toBe('i')
+    expect(treePrintArray[treePrintArray.length - 1]).toBe('a')
   })
 })
